5751215d19b0d7768b8fd6fc902d2ee8cf045a6a
Michel dos Santos <michelsantos15@gmail.com>
2020-09-02 13:05:39 -0300

    Adiciona a aula 02

---
+++ Aula 02 26:08/Aula01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aula01
+//
+//  Created by Michel dos Santos on 26/08/20.
+//  Copyright © 2020 Michel Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//
+}
+
---
+++ Aula 02 26:08/MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+//O índice é calculado da seguinte maneira: divide-se o peso do paciente pela sua altura elevada ao quadrado. Diz-se que o indivíduo tem peso normal quando o resultado do IMC está entre 18,5 e 24,9.
+
+
+//var altura:Float = 1.80
+//var peso: Float = 80
+//var imc: Float = peso / (altura*altura)
+//
+//
+//if(imc>=18.5 && imc<=24.9)
+//{
+//    print("Parabéns, peso normal")
+//}
+//print("Seu IMC é de \(imc)")
+
+
+var nome:String = "Michel"
+var sobrenome:String = "Santos"
+var padraoBr = nome + " " + sobrenome
+var padraoUs = sobrenome + " " + nome
+
+print("Nome no padão BR = \(padraoBr)")
+print("Nome no padão US = \(padraoUs)")
+
+    //Concatenar //String // 2 Variáveis, Nome e Sobrenome // Imprimir em padrão BR e US // Narlei Moreira // Moreira, Narlei
+
+
+
+
+
+
+
+
+
+
+
