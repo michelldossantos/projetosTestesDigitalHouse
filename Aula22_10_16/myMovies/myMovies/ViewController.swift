74a884af250ecae6d46de0d2d5650e4953e56c2f
Michel dos Santos <michelsantos15@gmail.com>
2020-10-19 23:03:32 -0300

    Challenge lesson 22

---
+++ Aula22_10_16/myMovies/myMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myMovies
+//
+//  Created by Michel dos Santos on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
