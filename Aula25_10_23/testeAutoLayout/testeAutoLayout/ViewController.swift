dfbbb5f762b9e81ff19a40b9feb7fbf350bca215
Michel dos Santos <michelsantos15@gmail.com>
2020-10-24 07:00:51 -0300

    Add lesson 25

---
+++ Aula25_10_23/testeAutoLayout/testeAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testeAutoLayout
+//
+//  Created by Michel dos Santos on 23/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("⭐️")
+    }
+
+  
+}
+
