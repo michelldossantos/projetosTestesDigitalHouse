1e1b2c7d823ab18f97ebf21f5dde73dc9f541d7b
Michel dos Santos <michelsantos15@gmail.com>
2021-01-23 01:16:13 -0300

    add exercicio ibay

--- Contents/Aula28_10_30_Sample_SOLID/10_30iBuy/10_30iBuy/Cell/ProductTableViewCell.swift
+++ Contents/Aula28_10_30_Sample_SOLID/10_30iBuy/10_30iBuy/Cell/ProductTableViewCell.swift
@@ -17,9 +17,14 @@ class ProductTableViewCell: UITableViewCell {
         labelName.text = product.name
     }
     
+    func setupOpen(product:Product){
+        
+        labelName.text = product.name
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
